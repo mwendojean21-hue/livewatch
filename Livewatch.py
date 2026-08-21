a9f09f5369cecf56927791a52d5acb3de03958bd
mwendojean21-hue <mwendojean21.com>
2026-08-21 11:22:26 +0200

    Fix logging for Vercel

--- Livewatch.py
+++ Livewatch.py
@@ -61,7 +61,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('livewatch.log', encoding='utf-8'),
         UnicodeStreamHandler()
     ]
 )
